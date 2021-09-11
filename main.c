c7ff8753307875ab022a247c3b3b563442b06f25
jaweokim <jaewokim@student.42seoul.kr>
2021-09-11 08:36:31 +0000

    change to samples on pdf

--- main.c
+++ main.c
@@ -18,13 +18,13 @@ void	rush(int x, int y);
 
 int	main(void)
 {
-	rush(4, 1);
+	rush(5, 3);
 	ft_putchar('\n');
-	rush(1, 4);
-	ft_putchar('\n');
-	rush(7, 3);
-	ft_putchar('\n');
-	rush(3, 6);
+	rush(5, 1);
 	ft_putchar('\n');
 	rush(1, 1);
+	ft_putchar('\n');
+	rush(1, 5);
+	ft_putchar('\n');
+	rush(4, 4);
 }
